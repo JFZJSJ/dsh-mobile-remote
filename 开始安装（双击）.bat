9c1157a665391de13bd94b0004945792040a62d1
鲍树森 <123566177+JFZJSJ@users.noreply.github.com>
2026-09-03 22:08:00 +0800

    更新: 开始安装（双击）.bat

--- 开始安装（双击）.bat
+++ 开始安装（双击）.bat
@@ -1,10 +1,10 @@
 @echo off
 chcp 65001 >nul
-title DSH 手机遥控台 · 一键安装
+title DSH �ֻ�ң��̨ �� һ����װ
 echo.
-echo   正在启动安装向导，请稍候……
-echo   （如果弹出"用户账户控制"窗口，请点【是】）
+echo   ����������װ�򵼣����Ժ򡭡�
+echo   ���������"�û��˻�����"���ڣ���㡾�ǡ���
 echo.
-powershell -NoProfile -ExecutionPolicy Bypass -File "%~dp0一键安装.ps1"
+powershell -NoProfile -ExecutionPolicy Bypass -File "%~dp0һ����װ.ps1"
 echo.
 pause
